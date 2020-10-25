3f1a5371904eb953fa03cc77f44fc220b888d07b
Chao Hao Xin <chaohao1@teach.cs.toronto.edu>
2020-10-25 16:50:44 -0400

    Fixed bug in runit.sh

--- a1b/runit.sh
+++ a1b/runit.sh
@@ -5,24 +5,24 @@ mkdir /tmp/chaohao1/dir1
 ls /tmp/chaohao1
 touch /tmp/chaohao1/dir1/file1
 echo "random content" >> /tmp/chaohao1/dir1/file1
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 echo "another file" > /tmp/chaohao1/dir1/file2
-ls /tmp/chaohao1/dir1/
-mkdir /tmp/chaohao1/dir/another_directory
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
+mkdir /tmp/chaohao1/dir1/another_directory
+ls /tmp/chaohao1/dir1
 cat /tmp/chaohao1/dir1/file1
 cat /tmp/chaohao1/dir1/file2
 unlink /tmp/chaohao1/dir1/file2
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 echo "add more content" >> /tmp/chaohao1/dir1/file1
 cat /tmp/chaohao1/dir1/file1
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 rmdir /tmp/chaohao1/dir1/another_directory
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 mkdir /tmp/chaohao1/dir1/a_new_dir
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 echo "a new file2" > /tmp/chaohao1/dir1/file2
-ls /tmp/chaohao1/dir1/
+ls /tmp/chaohao1/dir1
 fusermount -u /tmp/chaohao1
 ./a1fs disk_image /tmp/chaohao1
 ls /tmp/chaohao1
