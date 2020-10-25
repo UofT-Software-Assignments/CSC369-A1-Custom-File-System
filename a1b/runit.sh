5795efd82ecd2a558ff55a4daac99cf698387b29
Chao Hao Xin <chaohao1@teach.cs.toronto.edu>
2020-10-25 16:39:01 -0400

    fixed typo in script

--- a1b/runit.sh
+++ a1b/runit.sh
@@ -1,36 +1,31 @@
-make
 truncate -s 64K disk_image
 ./mkfs.a1fs -i 16 disk_image
 ./a1fs disk_image /tmp/chaohao1
-cd /tmp/chaohao1
-mkdir dir1
-ls
-cd dir1
-touch file1
-echo "random content" >> file1
-ls
-echo "another file" > file2
-ls
-mkdir another_directory
-ls
-cat file1
-cat file2
-unlink file2
-ls
-echo "add more content" >> file1
-cat file1
-ls
-rmdir another_directory
-ls
-mkdir a_new_dir
-ls
-echo "a new file2" > file2
-ls
+mkdir /tmp/chaohao1/dir1
+ls /tmp/chaohao1
+touch /tmp/chaohao1/dir1/file1
+echo "random content" >> /tmp/chaohao1/dir1/file1
+ls /tmp/chaohao1/dir1/
+echo "another file" > /tmp/chaohao1/dir1/file2
+ls /tmp/chaohao1/dir1/
+mkdir /tmp/chaohao1/dir/another_directory
+ls /tmp/chaohao1/dir1/
+cat /tmp/chaohao1/dir1/file1
+cat /tmp/chaohao1/dir1/file2
+unlink /tmp/chaohao1/dir1/file2
+ls /tmp/chaohao1/dir1/
+echo "add more content" >> /tmp/chaohao1/dir1/file1
+cat /tmp/chaohao1/dir1/file1
+ls /tmp/chaohao1/dir1/
+rmdir /tmp/chaohao1/dir1/another_directory
+ls /tmp/chaohao1/dir1/
+mkdir /tmp/chaohao1/dir1/a_new_dir
+ls /tmp/chaohao1/dir1/
+echo "a new file2" > /tmp/chaohao1/dir1/file2
+ls /tmp/chaohao1/dir1/
 fusermount -u /tmp/chaohao1
 ./a1fs disk_image /tmp/chaohao1
-cd /tmp/chaohao1
-ls
-cd dir1
-ls
-cat file1
-cat file2
+ls /tmp/chaohao1
+ls /tmp/chaohao1/dir1
+cat /tmp/chaohao1/dir1/file1
+cat /tmp/chaohao1/dir1/file2
